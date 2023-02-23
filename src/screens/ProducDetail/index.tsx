47f7837e376a6b967c3b0a4ba8037a3c40acaa09
weslley ferraz <weslleyhenrique800@gmail.com>
2023-02-23 13:36:49 -0400

    a

--- src/screens/ProducDetail/index.tsx
+++ src/screens/ProducDetail/index.tsx
@@ -16,6 +16,8 @@ import {
   Price,
   Title,
 } from './styles';
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/CartContext';
 
 interface Params {
   product: ProductsProps;
@@ -28,10 +30,20 @@ export function ProductDetail() {
   const price = product.price.toString().replace('.', ',');
   // const details = `Detalhes:\nPeso = ${product.weight}g\n, Comprimemento = ${product.length}cm\n, Largura = ${product.width}cm\n, Altura = ${product.height}cm`;
 
+  const cartContext = useContext(CartContext);
+
   function redirectToWhatsapp() {
     Linking.openURL(
       `https://api.whatsapp.com/send?phone=556533621007&text=Olá%20tenho%20interesse%20em%20comprar%20o%20produto:%20${product.name}`
     );
+  }
+
+  function handleAddToCart(id: number) {
+    cartContext.addProductToCart(id);
+  }
+
+  function handleRemoveToCart(id: number) {
+    cartContext.removeProductToCart(id);
   }
 
   return (
@@ -60,6 +72,20 @@ export function ProductDetail() {
             {/* <Descriptions>{details}</Descriptions> */}
           </ScrollView>
 
+          <Button
+            title="Adcionar ao Carrinho"
+            light
+            onPress={() => {
+              handleAddToCart(product.product_id);
+            }}
+          />
+          <Button
+            title="Remover do Carrinho"
+            light
+            onPress={() => {
+              handleRemoveToCart(product.product_id);
+            }}
+          />
           <Button title="Tenho interesse" light onPress={redirectToWhatsapp} />
         </ContainerDescriptions>
       </Content>
